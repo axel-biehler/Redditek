f6b328d846932c4d9052625e23c9a53dd6c84126
maxime <maxime@popay.io>
2021-10-27 15:11:47 +0200

    feature(HomeScroll): Add Scroll in Home Page

--- app/src/main/java/com/example/redditech/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/redditech/ui/home/HomeFragment.kt
@@ -4,17 +4,28 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.redditech.R
 import com.example.redditech.databinding.FragmentHomeBinding
+import kotlinx.android.synthetic.main.fragment_home.*;
 
 class HomeFragment : Fragment() {
 
+    private var titlesList = mutableListOf<String>()
+    private var descList = mutableListOf<String>()
+    private var imagesList = mutableListOf<Int>()
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -30,12 +41,32 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        postToList()
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(itemView: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(itemView, savedInstanceState)
+        rv_recyclerView.apply {
+            // set a LinearLayoutManager to handle Android
+            // RecyclerView behavior
+            layoutManager = LinearLayoutManager(activity)
+            // set the custom adapter to the RecyclerView
+            adapter = RecyclerAdapter(titlesList, descList, imagesList)
+        }
+    }
+
+    private fun addToList(title: String, description: String, image: Int) {
+        titlesList.add(title)
+        descList.add(description)
+        imagesList.add(image)
+    }
+
+    private fun postToList() {
+        for (i in 1..25) {
+            addToList("Title $i", "Description $i", R.mipmap.ic_launcher_round)
+        }
     }
 
     override fun onDestroyView() {
