bde04b8d11bd113177848847a8e8d68d5cfea2f8
maxime <maxime@popay.io>
2021-10-28 22:04:51 +0200

    feature(GlobalUI): add profil, change nav

--- app/src/main/java/com/example/redditech/MainActivity.kt
+++ app/src/main/java/com/example/redditech/MainActivity.kt
@@ -8,8 +8,6 @@ import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
-import okhttp3.*
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
